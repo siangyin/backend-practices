9e936c17514f7883726b5365fb3db1dc45dc14a0
Lee Siang Yin <siangyin916@gmail.com>
2021-12-25 16:36:28 +0800

    🏗 setup express-session, connectDB  🆗 adding basic routes & views-ejs setup

--- ex1-express-session/server.js
+++ ex1-express-session/server.js
@@ -1,23 +1,58 @@
+require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const MongoDBSession = require("connect-mongodb-session")(session);
+const mongoose = require("mongoose");
 const app = express();
 
-const SECRET = "heylapooyeah";
-const PORT = 3000;
+// Connect to Mongo
+mongoose.connect(process.env.MONGO_URI, (err) => {
+	if (err) throw err;
+	console.log("connected to MongoDB");
+});
+
+// Session collection data storing
+const store = new MongoDBSession({
+	uri: process.env.MONGO_URI,
+	collection: "ex1-Sessions",
+});
+
+// to access to views dir
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+// to access public dir such as css
+app.use(express.static("public"));
 
 app.use(
 	session({
-		secret: SECRET,
+		secret: process.env.SECRET,
 		resave: false,
 		saveUninitialized: false,
+		store: store,
 	})
 );
 
+//=================== Routes
+// Landing Page
 app.get("/", (req, res) => {
-	console.log(req.session);
-	res.send("hello");
+	// req.session.isAuth = true;
+	// console.log(req.session);
+	res.render("landing");
 });
 
-app.listen(PORT, () => {
-	console.log(`server on http://localhost:${PORT}`);
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+app.get("/dashboard", (req, res) => {
+	res.render("dashboard");
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`server on http://localhost:${process.env.PORT}`);
 });
